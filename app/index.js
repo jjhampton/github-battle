39e2fa077cd4c5b069bd69a3de6739c1dd5b51e3
John Hampton <john_j_hampton@yahoo.com>
2016-11-26 17:44:20 -0500

    Install Babel es2015 preset and add to webpack config
    Switch var syntax -> const
    Switch components to Stateless Functional Components

--- app/index.js
+++ app/index.js
@@ -1,49 +1,41 @@
-var USER_DATA = {
+const USER_DATA = {
     name: 'John Hamptones',
     username: 'jjhamptones',
     image: 'http://lorempixel.com/200/200/people/'
 }
 
-var React = require('react');
-var ReactDOM = require('react-dom');
+const React = require('react');
+const ReactDOM = require('react-dom');
 
-var ProfilePic = React.createClass({
-    render: function () {
-        return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />;
-    }
-});
+const ProfilePic = (props) => {
+    return <img src={props.imageUrl} style={{height: 100, width: 100}} />;
+};
 
-var ProfileLink = React.createClass({
-    render: function () {
-        return (
-            <div>   
-                <a href={'https://www.github.com' + this.props.username}>
-                    {this.props.username}
-                </a>
-            </div>
-        );
-    }
-});
+const ProfileLink = (props) => {
+    return (
+        <div>
+            <a href={`https://www.github.com${props.username}`}>
+                {props.username}
+            </a>
+        </div>
+    )
+};
 
-var ProfileName = React.createClass({
-    render: function () {
-        return (
-            <div>{this.props.name}</div>
-        );
-    }
-});
+const ProfileName = (props) => {
+    return (
+        <div>{props.name}</div>
+    );
+};
 
-var Avatar = React.createClass({
-    render: function() {
-        return (
-            <div>
-                <ProfilePic imageUrl={this.props.user.image} />
-                <ProfileName name={this.props.user.name} />
-                <ProfileLink username={this.props.user.username} />
-            </div>
-        );
-    }
-})
+const Avatar = (props) => {
+    return (
+        <div>
+            <ProfilePic imageUrl={props.user.image} />
+            <ProfileName name={props.user.name} />
+            <ProfileLink username={props.user.username} />
+        </div>
+    );
+};
 
 ReactDOM.render(
     <Avatar user={USER_DATA} />,
